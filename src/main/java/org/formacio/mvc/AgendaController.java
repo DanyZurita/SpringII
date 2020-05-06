fae89050308a6d86e2f8b3c64fb531e7c2420e45
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-06 15:57:29 +0200

    feat(OperationExpection): Añadir clase para captura de exception para caos test

--- src/main/java/org/formacio/mvc/AgendaController.java
+++ src/main/java/org/formacio/mvc/AgendaController.java
@@ -5,8 +5,11 @@ import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -32,8 +35,22 @@ public class AgendaController {
 	@RequestMapping(path="/contacte/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public Persona personaContacto(@PathVariable String id) {
-		return new Persona(id, agenda.recupera(id).getNom(), agenda.recupera(id).getTelefon());
+		if(agenda.recupera(id) == null){
+			throw new OperationException();
+		} 	
+		else
+			return agenda.recupera(id);
 	}
 	
-	
+	@RequestMapping(path="/afegir", method=RequestMethod.POST)
+	@ResponseBody
+	public String insertContacto(@RequestParam(name="id",
+								required=true) String id,
+								@RequestParam(name="nom",
+								required=true) String nom, 
+								@RequestParam(name="telefon",
+								required=true) String telefon) {
+		agenda.inserta(id, nom, telefon);
+		return "ok";
+	}
 }
